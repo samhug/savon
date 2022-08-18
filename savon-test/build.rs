87d57717a0612b7452b96090cf1b5a8b973c57bd
Sam Hug <s@m-h.ug>
2022-08-17 20:56:13 -0700

    .

--- savon-test/build.rs
+++ savon-test/build.rs
@@ -2,8 +2,6 @@ use std::env;
 
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
-    //let s = savon::gen::gen_write("../assets/example.wsdl", env!("OUT_DIR")).unwrap();
-    //let s = savon::gen::gen_write("../assets/example.wsdl", &out_dir).unwrap();
     savon::gen::gen_write("./CountryInfoService.wsdl", &out_dir).unwrap();
     savon::gen::gen_write("./example.wsdl", &out_dir).unwrap();
     savon::gen::gen_write("../assets/WHWebService.wsdl", &out_dir).unwrap();
